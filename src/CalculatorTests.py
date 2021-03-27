2684e7f38d9aeefcd4e7d66cc10bc5edfde7d091
Sally Khalil <sek22@njit.edu>
2021-03-27 00:10:35 -0400

    Fix: updated addition operation

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,9 +1,10 @@
 import unittest
 from Calculator import Calculator
+from CsvReader import CsvReader
+from pprint import pprint
 
 
 class MyTestCase(unittest.TestCase):
-
     def setUp(self) -> None:
         self.calculator = Calculator()
 
@@ -14,12 +15,15 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.result, 0)
 
     def test_add_method_calculator(self):
-        self.assertEqual(self.calculator.add(2, 2), 4)
-        self.assertEqual(self.calculator.result, 4)
+        test_data = CsvReader('/src/Unit Test Addition.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_subtract_method_calculator(self):
         self.assertEqual(self.calculator.subtract(2, 2), 0)
         self.assertEqual(self.calculator.result, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
